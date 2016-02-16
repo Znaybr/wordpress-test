efc86c3b966242611afb7f82550a1efc2fd953b7
tverjus <thibaut.verjus@gmail.com>
2016-02-16 12:12:55 -0500

    Ajout des images a la une et thumbnails

--- wp-content/themes/firstone/functions.php
+++ wp-content/themes/firstone/functions.php
@@ -2,6 +2,9 @@
 //Activation de l'onglet de menu dans l'interface WP
 add_theme_support("menus");
 
+//Activation de l'onglet de menu dans l'interface WP
+add_theme_support("post-thumbnails");
+
 //Déclaration de deux blocs de menus
 //Déclaration d'une fonction utilisateur qu'on va faire appeler par WP
 // - Une menu de header
@@ -18,3 +21,4 @@ function register_my_menus(){
 
 // Au démarrage (init) WP appel la fonction "register_my_menus"
 add_action('init', 'register_my_menus');
+
--- wp-content/themes/firstone/index.php
+++ wp-content/themes/firstone/index.php
@@ -3,11 +3,19 @@
 
     <?php while (have_posts()) : the_post(); ?>
         <article>
+
+<!--            VIGNETTE / IMAGE A LA UNE-->
+            <?php if(has_post_thumbnail()) { ?>
+                <p><?php the_post_thumbnail('thumbnail'); ?></p> <!--CHOIX : thumbnail, large ou full-->
+            <?php } ?>
+
+<!--            CONTENU DU RESUMÉ DE L'ARTICLE-->
             <h2><?php the_title(); ?></h2>
             <p><?php the_excerpt(); ?></p>
             <p> Article écrit par : <?php the_author(); ?> le <?php the_date(''); ?> </p>
             <a href="<?php the_permalink(); ?>" class="more"> Lire la suite </a>
             <p><?php comments_popup_link('Aucun commentaire', '1 commentaire', '% commentaires'); ?></p>
+
         </article>
     <?php endwhile; ?>
 </section>
